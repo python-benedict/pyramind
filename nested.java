90ec577f46544880a6833215dff2400c65f31f25
Benedict <newlyf38@gmail.com>
2022-08-26 07:13:23 +0000

    first commit

---
+++ nested.java
@@ -0,0 +1,23 @@
+/**
+ * nested
+ */
+import java.util.Scanner;
+public class nested {
+
+    public static void main(String[] args) {
+
+        
+        while (true) {
+            System.out.println("Enter the number of times you want in integer");
+            Scanner num1 = new Scanner(System.in);
+            int num = num1.nextInt();
+
+            for (int i = 0; i < num; i++) {
+                for (int j = 0; j < i; j++) {
+                    System.out.print("* ");
+                }
+                System.out.println("");
+            }
+        }
+    }
+}
